cacea9bd2f91f7aa283a9de9c4065bb2b517c0ca
basithahamed <basithhahamed@gmail.com>
2023-06-22 23:14:29 +0530

    Niriya vela pathuten bhaa solla ellathium solla mudiyadhu

---
+++ js/notification.js
@@ -0,0 +1,25 @@
+function showMenu() {
+    // document.getElementById("lmobile").style.display="block";
+    var x = document.getElementById("lmobile");   
+    if(x.style.display=='block')
+    {
+        x.style.display = 'none';
+
+    }
+    else
+    {
+        x.style.display = 'block';
+
+    }
+}
+
+
+// for the background Change
+function changeMe(y)
+{
+    var x=document.getElementById("pic")
+    console.log(y)
+    x.style.backgroundImage=`linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${y})`;
+
+}
+
--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-function showMenu() {
-    // document.getElementById("lmobile").style.display="block";
-    var x = document.getElementById("lmobile");   
-    if(x.style.display=='block')
-    {
-        x.style.display = 'none';
-
-    }
-    else
-    {
-        x.style.display = 'block';
-
-    }
-}
-
-
-// for the background Change
-function changeMe(y)
-{
-    var x=document.getElementById("pic")
-    console.log(y)
-    x.style.backgroundImage=`linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${y})`;
-
-}
-
 
 
 imageArr=['https://haikuholidays.com/images/1.jpeg','https://haikuholidays.com/images/2.jpeg']
